681a26dc0927cf31dd5c278739becbe44334b6e8
John Stuppy <stuppy.john@gmail.com>
2018-06-16 16:52:03 -0700

    maven archetype:generate
    mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app
    -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false

---
+++ src/main/java/com/idklomg/App.java
@@ -0,0 +1,13 @@
+package com.idklomg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
